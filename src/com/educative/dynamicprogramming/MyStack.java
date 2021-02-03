2391c05e21e2930eb7b76b46283f2f0797be1510
Narendra Darla <naren.darla@gmail.com>
2021-02-03 14:53:28 -0800

    Stack using Queue

---
+++ src/com/educative/dynamicprogramming/MyStack.java
@@ -0,0 +1,41 @@
+package com.educative.dynamicprogramming;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MyStack {
+
+    Queue<Integer> q1 = new LinkedList<>();
+    Queue<Integer> q2 = new LinkedList<>();
+    private int top ;
+    
+    public MyStack(){
+        
+    }
+    public void push(int x){
+        q1.add(x);
+        top = x;
+    }
+    
+    public int pop(){
+        int myTop = top;
+        while(q1.size()>1){
+            top = q1.remove();
+            q2.add(top);
+        }
+        q1.remove();
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+        return myTop;
+    }
+    
+    public int top(){
+        return top;
+    }
+    
+    
+    public boolean empty(){
+        return q1.isEmpty();
+    }
+}
